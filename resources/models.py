e012f7d815f4bdbf3a0546010aac0779236821ae
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-08 15:15:10 -0500

    created Category model with slug

--- resources/models.py
+++ resources/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -21,3 +23,22 @@ class Resource(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"slug": self.slug})
