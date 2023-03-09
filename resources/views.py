8c3832bf290bbc16ee5348a21c904b500a904041
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-09 10:23:31 -0500

    added user_passes_test to views.py

--- resources/views.py
+++ resources/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Resource
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .forms import ResourceForm
 
 
@@ -10,21 +10,21 @@ def list_resources(request):
     free_resources = Resource.objects.all().order_by('-created_at')
     for thing in free_resources:
         print(free_resources)
-
     return render(request, 'resources/index.html', {'free_resources': free_resources})
 
 
+@user_passes_test(lambda user: user.is_staff)
 def details_about_resource(request, pk):
     resource_description = get_object_or_404(Resource, pk=pk)
     return render(request, 'resources/details_about_resource.html', {'resource_description': resource_description})
 
 
+@user_passes_test(lambda user: user.is_staff)
 def add_resource(request):
     if request.method == 'POST':
         new_resource = ResourceForm(request.POST)
         if new_resource.is_valid():
             new_resource.save()
-            # creating a new instance of Album
             return redirect('home')
     form = ResourceForm()
     return render(request, 'resources/add_resource.html', {'form': form})
